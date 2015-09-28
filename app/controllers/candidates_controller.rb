2f909fce8f08952ff455400073cf744456f83c3c
dhemmat <dhemmat@gmail.com>
2015-09-27 20:15:37 -0400

    minor changes and basic ajax candidates show

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,4 +2,12 @@ class CandidatesController < ApplicationController
   def index
     @candidates = Candidate.all
   end
+
+  def show
+    @candidate = Candidate.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.js # show.js.erb
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Candidate.create([
+    {name: 'Peter Peterson', cv_link: 'http://www.google.com', owner_id: User.first.id},
+    {name: 'Mike Tyson', cv_link: 'http://www.google.com', owner_id: User.first.id},
+    {name: 'Tiger Woods', cv_link: 'http://www.google.com', owner_id: User.first.id},
+    {name: 'Woody Harrelson', cv_link: 'http://www.google.com', owner_id: User.first.id},
+  ])
